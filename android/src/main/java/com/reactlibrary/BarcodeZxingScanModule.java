a25072f9f2517e655e8ddfd56a3cb4d5b2216e34
Deeksha Mehta <deeksham@geekyants.com>
2022-01-05 15:49:56 +0530

    feat:scan method added

--- android/src/main/java/com/reactlibrary/BarcodeZxingScanModule.java
+++ android/src/main/java/com/reactlibrary/BarcodeZxingScanModule.java
@@ -2,13 +2,18 @@
 
 package com.reactlibrary;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.google.zxing.integration.android.IntentIntegrator;
 
-public class BarcodeZxingScanModule extends ReactContextBaseJavaModule {
+public class BarcodeZxingScanModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
     private final ReactApplicationContext reactContext;
 
@@ -25,11 +30,38 @@ public class BarcodeZxingScanModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void sampleMethod(String stringArgument, Promise promise) {
         // TODO: Implement some actually useful functionality
-       try {
-           promise.resolve(stringArgument);
-       } catch (Exception e) {
-           promise.reject(e);
-           //TODO: handle exception
-       }
+        try {
+            promise.resolve(stringArgument);
+        } catch (Exception e) {
+            promise.reject(e);
+            //TODO: handle exception
+        }
+    }
+
+
+    @ReactMethod
+    public void showQrReader(Callback callback) {
+        Callback mCallback = callback;
+        IntentIntegrator integrator = new IntentIntegrator(getCurrentActivity());
+        integrator.setOrientationLocked(true);
+        //  integrator.setCaptureActivity(ContinuousCaptureActivity.class);
+        integrator.initiateScan();
+        reactContext.addActivityEventListener(this);
+    }
+
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
     }
 }
